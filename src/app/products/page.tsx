9493e790561f562a30578fc1c18c6c167fd667b3
hr-shuvo <h.r.shuvo9@gmail.com>
2024-11-08 01:49:51 +0600

    product details

---
+++ src/app/products/page.tsx
@@ -0,0 +1,13 @@
+
+
+export default function ProductList(){
+    return(
+        <div>
+            <h1>product list</h1>
+            <p>product 1</p>
+            <p>product 2</p>
+            <p>product 3</p>
+        </div>
+
+    )
+}
